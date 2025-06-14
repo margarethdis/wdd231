03fad571d5f37b084a332c91d547d6daabfa3f7e
margarethdis <margarethdani2021@gmail.com>
2025-06-14 16:04:46 -0600

    ty

---
+++ chamber/script/modal.js
@@ -0,0 +1,18 @@
+function openModal(id) {
+    document.getElementById(id).classList.remove('hidden');
+  }
+
+  function closeModal(id) {
+    document.getElementById(id).classList.add('hidden');
+  }
+
+  // Opcional: cerrar modal al hacer clic fuera
+  window.onclick = function(event) {
+    const modals = document.querySelectorAll('.modal');
+    modals.forEach(modal => {
+      if (event.target === modal) {
+        modal.classList.add('hidden');
+      }
+    });
+  }
+
